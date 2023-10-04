ccfc83c52109930141cd9496c3e87036fe9f5edf
FindWhatYou <125308150+FindWhatYou@users.noreply.github.com>
2023-10-04 19:49:38 +0200

    value

--- popular_app/app.py
+++ popular_app/app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 df = pd.read_csv(r'./popular_app/best_movies.csv') 
 
-number = st.number_input('Insert a number', step=1)
+number = st.number_input('Insert a number', step=1, value=5)
 displayed_df = df[:number]
 
 st.data_editor(displayed_df, num_rows="dynmic", hide_index=True)
