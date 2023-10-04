9d67b82df6b2f975425928c1c6f802da4bab9f6b
FindWhatYou <125308150+FindWhatYou@users.noreply.github.com>
2023-10-04 20:11:18 +0200

    first working version

--- popular_app/app.py
+++ popular_app/app.py
@@ -4,12 +4,7 @@ import streamlit as st
 
 df = pd.read_csv(r'./popular_app/best_movies.csv') 
 
-number = st.number_input('Insert a number', step=1, min_value=2, value=2)
+number = st.number_input('How many top movies do you want to see?', step=1, min_value=2, value=2)
 displayed_df = df[:number]
 
 st.data_editor(displayed_df, num_rows="dynmic", hide_index=True)
-
-
-
-
-
