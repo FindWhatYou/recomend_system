a723b2d3b5227e9dbae907a193ee0d958bb8cd50
FindWhatYou <125308150+FindWhatYou@users.noreply.github.com>
2023-10-04 19:12:44 +0200

    dir

--- popular_app/app.py
+++ popular_app/app.py
@@ -2,7 +2,8 @@ import pandas as pd
 import streamlit as st
 
 
-df = pd.read_csv(r'popular_app/best_movies.csv') 
+df = pd.read_csv(r'./popular_app/best_movies.csv') 
+print(df)
 
 st.data_editor(df)
 
