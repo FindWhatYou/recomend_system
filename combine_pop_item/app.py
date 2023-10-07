40eec782496e6a1977f450cb3d4ec4f775365fc2
FindWhatYou <125308150+FindWhatYou@users.noreply.github.com>
2023-10-07 11:21:38 +0200

    fux tabel

--- combine_pop_item/app.py
+++ combine_pop_item/app.py
@@ -7,4 +7,4 @@ df = pd.read_csv(r'./popular_app/best_movies.csv')
 number = st.number_input('How many top movies do you want to see?', step=1, min_value=2, value=2)
 displayed_df = df[:number]
 
-st.table(displayed_df, num_rows="dynmic", hide_index=True)
+st.table(displayed_df)
