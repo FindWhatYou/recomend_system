fa326bc702e608c285701ae0b64ed52f041f7b47
FindWhatYou <125308150+FindWhatYou@users.noreply.github.com>
2023-10-07 11:28:16 +0200

    Add Title

--- combine_pop_item/app.py
+++ combine_pop_item/app.py
@@ -4,6 +4,9 @@ import streamlit as st
 
 df = pd.read_csv(r'./popular_app/best_movies.csv') 
 
+st.title("Welcome to WBSFLIX")
+st.markdown("##Most Popupal Movies Ever")
+
 number = st.number_input('How many top movies do you want to see?', step=1, min_value=2, value=2)
 displayed_df = df[:number]
 
